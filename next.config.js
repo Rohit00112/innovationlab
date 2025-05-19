f3c285e15dec44dd3038c2cabfe66ebf417f7d7d
Sujan Subedi <subedirohit49@gmail.com>
2025-05-19 15:52:04 +0545

    Disable ESLint for Netlify build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     domains: ['placehold.co', 'images.unsplash.com', 'source.unsplash.com', 'plus.unsplash.com', 'images.nativeplanet.com'],
   },
+  // Disable ESLint during build
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
