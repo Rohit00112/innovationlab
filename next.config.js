89957859d4f2b64b0c0b06698b16eff7b53a2298
Sujan Subedi <subedirohit49@gmail.com>
2025-05-19 15:55:40 +0545

    disable lint for typescript

--- next.config.js
+++ next.config.js
@@ -10,6 +10,12 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  // Disable TypeScript type checking during build
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
